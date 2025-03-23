ea9f41ead9b033f7a401d94a2e68a1be6e5efafd
Bahauddinmohammed <61136445+Bahauddinmohammed@users.noreply.github.com>
2025-03-23 16:38:27 +0530

    added UI changes in home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'register',
     component: RegistrationComponent
-}
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  cardDetails = [
+    {
+      title: 'Covid-19 Updates',
+      subtitle: 'Stay informed about the latest COVID-19 guidelines and vaccination information',
+      btntext: 'Learn More'
+    }
+  ];
+  ngOnInit(): void {
+    {
+     console.log(this.cardDetails,'ddd') 
+    }
+  }
 }
